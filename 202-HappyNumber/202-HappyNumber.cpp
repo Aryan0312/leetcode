c3707cf0de5d278b443b35cea2d3d1964d7f0930
Aryan Shrivastav <109823129+Aryan0312@users.noreply.github.com>
2026-08-17 09:23:34 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/202-HappyNumber.cpp
@@ -0,0 +1,23 @@
+// Last updated: 8/17/2026, 9:23:33 AM
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> s;
+        while(true){
+
+            s.insert(n);
+        int sum = 0;
+            while(n!=0){
+                sum += (n%10)*(n%10);
+                n = n/10;
+            }
+            if(sum == 1){
+                return true;
+            }
+            if(s.count(sum)){
+                return false;
+            }
+            n = sum;
+        }
+    }
+};
