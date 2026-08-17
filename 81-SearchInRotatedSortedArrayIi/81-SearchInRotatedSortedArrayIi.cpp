6fa31b1a74b0d26f45b673a78bda4c4c36747d8f
Aryan Shrivastav <109823129+Aryan0312@users.noreply.github.com>
2026-08-17 09:24:06 +0530

    Create 81-SearchInRotatedSortedArrayIi

---
+++ 81-SearchInRotatedSortedArrayIi/81-SearchInRotatedSortedArrayIi.cpp
@@ -0,0 +1,40 @@
+// Last updated: 8/17/2026, 9:24:06 AM
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        int low = 0;
+        int high = n-1;
+
+        while(low<=high){
+            int mid = low+(high-low)/2;
+
+
+            if(nums[mid] == target){
+                return true;
+            }
+            if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+                low = low+1;
+                high = high -1;
+                continue;
+            }
+            
+            if(nums[low] <= nums[mid]){
+                if(target <= nums[mid] && target >= nums[low]){
+                    high = mid-1;
+                }else{
+                    low = mid+1;
+                }
+            }else{
+                if(target >= nums[mid] && target <= nums[high]){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
+            }
+        }
+
+        return false;
+    }
+};
