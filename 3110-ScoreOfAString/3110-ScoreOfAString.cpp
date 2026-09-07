ffc4c3deb1298119915e6a9999998d4af7256bdf
Aryan Shrivastav <109823129+Aryan0312@users.noreply.github.com>
2026-09-07 23:09:28 +0530

    Create 3110-ScoreOfAString

---
+++ 3110-ScoreOfAString/3110-ScoreOfAString.cpp
@@ -0,0 +1,13 @@
+// Last updated: 9/7/2026, 11:09:28 PM
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int sum = 0;
+        for(int i =0;i<s.size()-1;i++){
+            sum+= abs(int(s[i]) - int(s[i+1]));
+        }
+
+
+        return sum;
+    }
+};
