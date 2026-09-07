86971911f69e2424ed78e6a11142a507f617608f
Aryan Shrivastav <109823129+Aryan0312@users.noreply.github.com>
2026-09-07 23:11:09 +0530

    Create 1920-BuildArrayFromPermutation

---
+++ 1920-BuildArrayFromPermutation/1920-BuildArrayFromPermutation.cpp
@@ -0,0 +1,14 @@
+// Last updated: 9/7/2026, 11:11:08 PM
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+
+        for(int i =0;i<n;i++){
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+};
