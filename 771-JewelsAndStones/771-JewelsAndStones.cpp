03acd582aa958fb510db289eac53c0fb6b36bd0c
Aryan Shrivastav <109823129+Aryan0312@users.noreply.github.com>
2026-09-07 23:12:54 +0530

    Create 771-JewelsAndStones

---
+++ 771-JewelsAndStones/771-JewelsAndStones.cpp
@@ -0,0 +1,18 @@
+// Last updated: 9/7/2026, 11:12:53 PM
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int n = jewels.size();
+        int m = stones.size();
+        int count = 0;
+        for(int i =0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(jewels[i] == stones[j]){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
