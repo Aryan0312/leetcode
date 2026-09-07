c22bf9a86ca84523973b298cf5e1f53b1d96edf1
Aryan Shrivastav <109823129+Aryan0312@users.noreply.github.com>
2026-09-07 23:11:29 +0530

    Create 1672-RichestCustomerWealth

---
+++ 1672-RichestCustomerWealth/1672-RichestCustomerWealth.cpp
@@ -0,0 +1,24 @@
+// Last updated: 9/7/2026, 11:11:29 PM
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        
+        int n = accounts.size();
+        int m = accounts[0].size();
+
+        int maxAmount = INT_MIN;
+
+        for(int i =0;i<n;i++){
+
+            int sum = 0;
+
+            for(int j = 0;j<m;j++){
+                sum += accounts[i][j];
+            }
+            maxAmount = max(sum,maxAmount);
+        }
+
+        return maxAmount;
+
+    }
+};
