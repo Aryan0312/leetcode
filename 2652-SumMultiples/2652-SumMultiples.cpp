c2883bcee12c6597a6099b0c09b6d2d23925c042
Aryan Shrivastav <109823129+Aryan0312@users.noreply.github.com>
2026-09-07 23:09:58 +0530

    Create 2652-SumMultiples

---
+++ 2652-SumMultiples/2652-SumMultiples.cpp
@@ -0,0 +1,14 @@
+// Last updated: 9/7/2026, 11:09:58 PM
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        int sum = 0;
+        for(int i =0;i<=n;i++){
+            if(i % 3 == 0 || i % 5 == 0 || i % 7 == 0){
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+};
