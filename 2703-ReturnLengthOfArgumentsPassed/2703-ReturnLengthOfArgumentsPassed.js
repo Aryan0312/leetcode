31da8f55037e707c86fe352113a3c018487340f9
Aryan Shrivastav <109823129+Aryan0312@users.noreply.github.com>
2026-09-07 23:09:54 +0530

    Create 2703-ReturnLengthOfArgumentsPassed

---
+++ 2703-ReturnLengthOfArgumentsPassed/2703-ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,13 @@
+// Last updated: 9/7/2026, 11:09:53 PM
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+    
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
