c12c8bc845742283e20aa59ddf246288be380f2f
Aryan Shrivastav <109823129+Aryan0312@users.noreply.github.com>
2026-09-07 23:12:27 +0530

    Create 1313-DecompressRunLengthEncodedList

---
+++ 1313-DecompressRunLengthEncodedList/1313-DecompressRunLengthEncodedList.cpp
@@ -0,0 +1,18 @@
+// Last updated: 9/7/2026, 11:12:26 PM
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> ans;
+        for(int i =0;i<n;i+=2){
+            int freq = nums[i];
+            int val = nums[i+1];
+            for(int j = 0;j<freq;j++){
+                ans.push_back(val);
+            }
+        }
+
+        return ans;
+    }
+};
