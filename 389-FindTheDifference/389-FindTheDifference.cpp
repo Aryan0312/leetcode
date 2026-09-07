aa65c9452f5e9e18abdda0ed806df4943e07127f
Aryan Shrivastav <109823129+Aryan0312@users.noreply.github.com>
2026-09-07 22:51:26 +0530

    Create 389-FindTheDifference

---
+++ 389-FindTheDifference/389-FindTheDifference.cpp
@@ -0,0 +1,20 @@
+// Last updated: 9/7/2026, 10:51:26 PM
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+
+        for(int i =0;i<n;i++){
+            if(s[i] != t[i]){
+                return t[i];
+            }
+        }
+
+        return t.back();
+
+    }
+};
