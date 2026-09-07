faf7e6e96e8ca420404ea972f2fa465fc02b4394
Aryan Shrivastav <109823129+Aryan0312@users.noreply.github.com>
2026-09-07 23:11:25 +0530

    Create 1678-GoalParserInterpretation

---
+++ 1678-GoalParserInterpretation/1678-GoalParserInterpretation.cpp
@@ -0,0 +1,27 @@
+// Last updated: 9/7/2026, 11:11:25 PM
+class Solution {
+
+
+public:
+    string interpret(string command) {
+        int n = command.size();
+        
+        string ans = "";
+
+        for(int i =0;i<n;i++){
+            
+            if(command[i] == 'G'){
+                ans += 'G';
+            }else if(command[i] == '(' && command[i+1] == ')'){
+                ans+= 'o';
+                i++;
+            }else{
+                ans+="al";
+                i+=3;
+            }
+            
+        }
+
+        return ans;
+    }
+};
